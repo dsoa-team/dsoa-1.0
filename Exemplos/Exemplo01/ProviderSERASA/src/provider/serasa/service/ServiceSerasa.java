98901c2a0d9c286a617412d11a90ab345b5a8b48
tacsio <tcs5cin@gmail.com>
2012-02-11 11:20:55 -0300

    organizando repositório

--- Exemplos/Exemplo01/ProviderSERASA/src/provider/serasa/service/ServiceSerasa.java
+++
@@ -1,8 +0,0 @@
-package provider.serasa.service;
-
-public interface ServiceSerasa {
-
-	public boolean searchStateSerasa(String name);
-	public String  searchCauseSerasa(String name);
-	
-}
--- Exemplos/Exemplo01/ProviderSPC/src/provider/spc/service/ServiceSpc.java
+++
@@ -1,8 +0,0 @@
-package provider.spc.service;
-
-public interface ServiceSpc {
-
-	public boolean searchStateSpc(String name);
-	public String  searchCauseSpc(String name);
-
-}
